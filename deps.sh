6d501d71acc3dd85751d5996ad60da02379534b8
Lukas Hönig <lukas.hoenig@trademob.com>
2014-02-20 22:14:51 +0100

    Installer script for the perl dependencies using cpan

---
+++ deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+
+# install perl dependency modules
+
+PERL_MM_USE_DEFAULT=1; 
+sudo perl -MCPAN -e 'install JSON, Term::ANSIColor, LWP::UserAgent, Getopt::Long, URI::Encode;'
